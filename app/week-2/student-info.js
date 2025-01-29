c42e43f5e16a47ce6162db77eb25f8f0664fa065
ruchi079 <RuchiJItendrakumar.Panseriya@edu.sait.ca>
2025-01-28 19:57:28 -0700

    Week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+"use client";
+
+
+const StudentInfo = () => {
+  return (
+    <div className="p-4 border rounded-lg shadow-md">
+      <h2 className="text-xl font-bold">Student Information</h2>
+      <p className="mt-2">Name: Ruchi Panseriya</p>
+    </div>
+  );
+};
+
+export default StudentInfo;
